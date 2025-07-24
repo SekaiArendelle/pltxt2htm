acf94e5ed5c78475e1e57590e5bdda8a96996384
Arendelle <2381642961@qq.com>
2025-07-24 15:05:40 +0800

    Add x86_64-w64-mingw32 ci

--- test/0025.fuzzer_leak.cc
+++ test/0025.fuzzer_leak.cc
@@ -3,6 +3,9 @@
 #include <pltxt2htm/pltxt2htm.hh>
 
 int main() noexcept {
-    ::fast_io::u8string_view data{u8R"(#oo<Color=<s�<s<s�s�/<h1>e<\�����������<sa\/<h1>�s�/<h1>e<h1>Ns<<�  �����<as\/<h1>�s�/<h1>e<\�������������������<sa\/<h1>Ns<<?���������<sa�/<h1>�s=<s�<s<s�s�/<h1>e<\����TTTTTTTTTTTTT�������>sa\/<h1>�s�/<h1>e<\�����������������<sa\/<h1>Ns<<�  �����<sa\/<h1>�s�/<h1>e���������<s\oo<Color��)"};
-    [[maybe_unused]] auto _ = ::pltxt2htm::pltxt2advanced_html(data, u8"");
+    ::fast_io::u8string_view data{
+        u8R"(#oo<Color=<s�<s<s�s�/<h1>e<\�����������<sa\/<h1>�s�/<h1>e<h1>Ns<<�  �����<as\/<h1>�s�/<h1>e<\�������������������<sa\/<h1>Ns<<?���������<sa�/<h1>�s=<s�<s<s�s�/<h1>e<\����TTTTTTTTTTTTT�������>sa\/<h1>�s�/<h1>e<\�����������������<sa\/<h1>Ns<<�  �����<sa\/<h1>�s�/<h1>e���������<s\oo<Color��)"};
+    [[maybe_unused]] auto _ = ::pltxt2htm::pltxt2advanced_html(data, u8"_");
+
+    return 0;
 }
