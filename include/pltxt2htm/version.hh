58242d1afe77da27e4a37f2248b1e06f603bf8c7
Arendelle <2381642961@qq.com>
2025-07-30 16:06:26 +0800

    pltxt2htm v0.2.0

--- include/pltxt2htm/version.hh
+++ include/pltxt2htm/version.hh
@@ -7,7 +7,7 @@ namespace pltxt2htm::version {
 // For breaking change
 constexpr ::std::size_t major{0};
 // For new features without breaking change
-constexpr ::std::size_t minor{1};
+constexpr ::std::size_t minor{2};
 // For bug fixes without new features
 constexpr ::std::size_t patch{0};
 
