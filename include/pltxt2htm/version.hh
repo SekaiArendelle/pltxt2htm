e677533e4560b0dd2454d8efdbf8a2b2b627cda4
Arendelle <Arendelle_@outlook.com>
2025-08-17 13:53:06 +0800

    pltxt2htm v0.3.0
    * Experimental release ci

--- include/pltxt2htm/version.hh
+++ include/pltxt2htm/version.hh
@@ -7,7 +7,7 @@ namespace pltxt2htm::version {
 // For breaking change
 inline constexpr ::std::size_t major{0};
 // For new features without breaking change
-inline constexpr ::std::size_t minor{2};
+inline constexpr ::std::size_t minor{3};
 // For bug fixes without new features
 inline constexpr ::std::size_t patch{0};
 
