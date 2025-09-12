8d795a46866c904449a91879b79bc467f858a4bc
Arendelle <2381642961@qq.com>
2025-09-12 22:53:32 +0800

    more test of md del

--- test/0038.md_del.cc
+++ test/0038.md_del.cc
@@ -13,5 +13,8 @@ int main() {
     auto answer3 = ::fast_io::u8string_view{u8"te~~st<br>~~"};
     ::pltxt2htm_test::assert_true(html3 == answer3);
 
+    auto html4 = ::pltxt2htm_test::pltxt2advanced_htmld(u8"t~~e~~st");
+    auto answer4 = ::fast_io::u8string_view{u8"t<del>e</del>st"};
+    ::pltxt2htm_test::assert_true(html4 == answer4);
     return 0;
 }
