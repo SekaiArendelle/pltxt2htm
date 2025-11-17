f3251a59e952d81ee4e62573dc67597ffc4adfe1
Arendelle <Arendelle_@outlook.com>
2025-11-17 21:19:51 +0800

    More test to heap_guard

--- test/0049.heap_guard.cc
+++ test/0049.heap_guard.cc
@@ -1,10 +1,24 @@
 #include <pltxt2htm/heap_guard.hh>
+#include <fast_io/fast_io_dsal/vector.h>
+#include <fast_io/fast_io_dsal/list.h>
 
 int main() {
-    ::pltxt2htm::HeapGuard<int> a{};
-    ::pltxt2htm::HeapGuard<int> b{};
-    ::pltxt2htm::HeapGuard<int> c{1};
+    {
+        ::pltxt2htm::HeapGuard<int> a{};
+        ::pltxt2htm::HeapGuard<int> b{};
+        ::pltxt2htm::HeapGuard<int> c{1};
 
-    ::exception::assert_true<false>(a == b);
-    ::exception::assert_true<false>(a != c);
+        ::exception::assert_true<false>(a == b);
+        ::exception::assert_true<false>(a != c);
+    }
+    {
+        ::fast_io::vector<::pltxt2htm::HeapGuard<int>> a{};
+        ::fast_io::vector<::pltxt2htm::HeapGuard<int>> b{};
+        ::exception::assert_true<false>(a == b);
+    }
+    {
+        ::fast_io::list<::pltxt2htm::HeapGuard<int>> a{};
+        ::fast_io::list<::pltxt2htm::HeapGuard<int>> b{};
+        ::exception::assert_true<false>(a == b);
+    }
 }
