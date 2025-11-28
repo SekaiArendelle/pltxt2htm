ae4ec36f6ec0f3ba444bad87d7d05698fed5f805
Arendelle <Arendelle_@outlook.com>
2025-11-28 11:07:20 +0800

    Remove note node in optimize_ast (#97)
    * pltxt2htm v0.7.0
    
    * Adjust loop in optimize_ast
    
    * Remove note node in optimizer

--- include/pltxt2htm/version.hh
+++ include/pltxt2htm/version.hh
@@ -8,8 +8,8 @@ namespace pltxt2htm::version {
 // For breaking change
 inline constexpr ::std::size_t major{0};
 // For new features without breaking change
-inline constexpr ::std::size_t minor{6};
+inline constexpr ::std::size_t minor{7};
 // For bug fixes without new features
-inline constexpr ::std::size_t patch{1};
+inline constexpr ::std::size_t patch{0};
 
 } // namespace pltxt2htm::version
