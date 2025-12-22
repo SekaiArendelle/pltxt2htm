67dcb6418c76e513c1670f5898148b7707fa0b67
Arendelle <Arendelle_@outlook.com>
2025-12-22 16:43:15 +0800

    Drop format.py
    - add format_cpp.ninja
    - add format_xmake.py

---
+++ format_xmake.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+
+def format_files_in_a_dir(dir: str) -> None:
+    for root, dirs, files in os.walk(dir):
+        for file in files:
+            if file == "xmake.lua":
+                cmd = f'lua-format -i "{os.path.join(root, file)}"'
+                os.system(cmd)
+                print(">> ", cmd)
+
+def main():
+    SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+    # lua-format from https://github.com/Koihik/LuaFormatter
+    if not shutil.which("lua-format"):
+        raise Exception("lua-format not found")
+
+    format_files_in_a_dir(SCRIPT_DIR)
+
+if __name__ == "__main__":
+    main()
