685e684ba7a3aaf8caf1d68332a7c442dff15cf0
Arendelle <Arendelle_@outlook.com>
2026-01-27 18:20:20 +0800

    pltxt2htm v0.13.0

--- include/pltxt2htm/version.hh
+++ include/pltxt2htm/version.hh
@@ -20,7 +20,7 @@ inline constexpr ::std::size_t major{0};
  * @brief Minor version number - incremented for new features without breaking changes
  * @note New features that are backward compatible increment this number
  */
-inline constexpr ::std::size_t minor{12};
+inline constexpr ::std::size_t minor{13};
 
 /**
  * @brief Patch version number - incremented for bug fixes without new features
