46ff7fd4fa1f894174b522074a0baf43c6eda184
Arendelle <Arendelle_@outlook.com>
2026-04-14 16:03:03 +0800

    enable a commented test

--- tests/0046.md_link.cc
+++ tests/0046.md_link.cc
@@ -50,9 +50,9 @@ int main() {
     }
 
     {
-        // auto html = ::pltxt2htm_test::pltxt2advanced_htmld(u8"[nested [link]](example.com)");
-        // auto answer = ::fast_io::u8string_view{u8"[nested&nbsp;[link]](url)"};
-        // ::pltxt2htm_test::assert_true(html == answer);
+        auto html = ::pltxt2htm_test::pltxt2advanced_htmld(u8"[nested [link]](example.com)");
+        auto answer = ::fast_io::u8string_view{u8"[nested&nbsp;[link]](example.com)"};
+        ::pltxt2htm_test::assert_true(html == answer);
     }
 
     {
