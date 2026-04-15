aa14cb7670b549d9aca16faf148259d6316bc433
Arendelle <Arendelle_@outlook.com>
2026-04-15 14:18:47 +0800

    test: cover all markdown escapes in image alt text

--- tests/0061.md_image.cc
+++ tests/0061.md_image.cc
@@ -127,6 +127,19 @@ int main() {
     }
 
     {
+        // Cover all markdown backslash escapes in alt text.
+        auto pltext = ::fast_io::u8string_view{
+            u8R"(![\\\!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\]\^\_\`\{\|\}\~](example.com/image.jpg))"};
+        auto html = ::pltxt2htm_test::pltxt2advanced_htmld(pltext);
+        auto answer = ::fast_io::u8string_view{
+            u8"<img src=\"example.com/image.jpg\" alt=\"\\!&quot;#$%&amp;&apos;()*+,-./:;&lt;=&gt;?@[]^_`{|}~\">"};
+        ::pltxt2htm_test::assert_true(html == answer);
+        auto plunity_richtext = ::pltxt2htm_test::pltxt2plunity_introduction(pltext);
+        auto plunity_richtext_answer = pltext;
+        ::pltxt2htm_test::assert_true(plunity_richtext == plunity_richtext_answer);
+    }
+
+    {
         auto pltext = ::fast_io::u8string_view{u8"text![text](example.com/image.jpg)text"};
         auto html = ::pltxt2htm_test::pltxt2advanced_htmld(pltext);
         auto answer = ::fast_io::u8string_view{u8"text<img src=\"example.com/image.jpg\" alt=\"text\">text"};
