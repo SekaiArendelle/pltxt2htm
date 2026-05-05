3097e39a7a87387e66c487a3437a5a87c2bea470
Arendelle <Arendelle_@outlook.com>
2026-05-05 20:58:00 +0800

    fix cxxmodule/

--- cxxmodule/example.cc
+++ cxxmodule/example.cc
@@ -2,10 +2,11 @@ import fast_io;
 import pltxt2htm;
 
 int main() {
-    auto html = ::pltxt2htm::pltxt2advanced_html(u8R"(
+    auto html = ::pltxt2htm::pltxt2fixedadv_html(u8R"(
 # Hello Quantum PhysicsLab
 With Markdown supports
-)");
+)",
+    u8"localhost:5173", u8"$PROJECT", u8"$VISITOR", u8"$AUTHOR", u8"$CO_AUTHORS");
     ::fast_io::io::println(::fast_io::u8c_stdout(), html);
 
     return 0;
