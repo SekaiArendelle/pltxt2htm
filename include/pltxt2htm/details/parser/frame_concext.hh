84d52d8b8a55d9d5420a65a3a6035f56cd0ca2d1
Arendelle <Arendelle_@outlook.com>
2026-05-06 12:50:07 +0800

    remove ContextDataKind

--- include/pltxt2htm/details/parser/frame_concext.hh
+++ include/pltxt2htm/details/parser/frame_concext.hh
@@ -9,7 +9,6 @@
 #pragma once
 
 #include <cstddef>
-#include <cstdint>
 #include <memory>
 #include <utility>
 #include <exception/exception.hh>
@@ -21,8 +20,6 @@
 #include "../push_macro.hh"
 
 namespace pltxt2htm::details {
-
-class ParserFrameContextWithoutInfo {};
 
 class ParserFrameContextWithPltextInfo {
 public:
@@ -72,19 +69,7 @@ public:
 template<::pltxt2htm::Contracts ndebug>
 class BasicFrameContext {
 public:
-    enum class ContextDataKind : ::std::uint_least8_t {
-        without_info = 0,
-        pltext,
-        equal_sign_tag,
-        external_tag,
-        pl_size_tag,
-        md_block_quotes,
-        md_link,
-        md_list,
-    };
-
     union ContextVariant {
-        ::pltxt2htm::details::ParserFrameContextWithoutInfo without_info;
         ::pltxt2htm::details::ParserFrameContextWithPltextInfo pltext;
         ::pltxt2htm::details::ParserFrameContextWithEqualSignTagInfo equal_sign_tag;
         ::pltxt2htm::details::ParserFrameContextWithExternalTagInfo external_tag;
@@ -94,7 +79,7 @@ public:
         ::pltxt2htm::details::ParserFrameContextWithMdListInfo md_list;
 
         constexpr ContextVariant() noexcept
-            : without_info{} {
+            : pltext{} {
         }
 
         constexpr ContextVariant(::pltxt2htm::details::ParserFrameContextWithPltextInfo&& pltext_context) noexcept
@@ -133,73 +118,81 @@ public:
         }
     } context_data;
 
-    ::pltxt2htm::NodeType const nested_tag_type;
+    ::pltxt2htm::NodeType nested_tag_type;
     ::std::size_t current_index{};
     ::pltxt2htm::Ast subast{};
-    ContextDataKind context_data_kind;
 
     constexpr explicit BasicFrameContext(::pltxt2htm::NodeType const nested_tag_type_) noexcept
         : context_data{},
-          nested_tag_type{nested_tag_type_},
-          context_data_kind{ContextDataKind::without_info} {
+          nested_tag_type{nested_tag_type_} {
+        bool const is_plain_pltext_type{
+            ::pltxt2htm::details::is_plain_pltext_type(nested_tag_type_)};
+        pltxt2htm_assert(is_plain_pltext_type, u8"mismatch node type");
     }
 
     constexpr explicit BasicFrameContext(::fast_io::u8string_view pltext_,
                                          ::pltxt2htm::NodeType const nested_tag_type_) noexcept
         : context_data{::pltxt2htm::details::ParserFrameContextWithPltextInfo{pltext_}},
-          nested_tag_type{nested_tag_type_},
-          context_data_kind{ContextDataKind::pltext} {
+          nested_tag_type{nested_tag_type_} {
+        bool const is_plain_pltext_type{
+            ::pltxt2htm::details::is_plain_pltext_type(nested_tag_type_)};
+        pltxt2htm_assert(is_plain_pltext_type, u8"mismatch node type");
     }
 
     constexpr explicit BasicFrameContext(::fast_io::u8string_view pltext_, ::pltxt2htm::NodeType const nested_tag_type_,
                                          ::pltxt2htm::Ast&& subast_) noexcept
         : context_data{::pltxt2htm::details::ParserFrameContextWithPltextInfo{pltext_}},
           nested_tag_type{nested_tag_type_},
-          subast(::std::move(subast_)),
-          context_data_kind{ContextDataKind::pltext} {
+          subast(::std::move(subast_)) {
+        bool const is_plain_pltext_type{
+            ::pltxt2htm::details::is_plain_pltext_type(nested_tag_type_)};
+        pltxt2htm_assert(is_plain_pltext_type, u8"mismatch node type");
     }
 
     constexpr explicit BasicFrameContext(::fast_io::u8string_view pltext_, ::pltxt2htm::NodeType const nested_tag_type_,
                                          ::fast_io::u8string&& id_) noexcept
         : context_data{
               ::pltxt2htm::details::ParserFrameContextWithEqualSignTagInfo{.pltext = pltext_, .id = ::std::move(id_)}},
-          nested_tag_type{nested_tag_type_},
-          context_data_kind{ContextDataKind::equal_sign_tag} {
+          nested_tag_type{nested_tag_type_} {
+        bool const is_equal_sign_tag_type{
+            ::pltxt2htm::details::is_equal_sign_tag_type(nested_tag_type_)};
+        pltxt2htm_assert(is_equal_sign_tag_type, u8"mismatch node type");
     }
 
     constexpr explicit BasicFrameContext(::fast_io::u8string_view pltext_, ::pltxt2htm::NodeType const nested_tag_type_,
                                          ::pltxt2htm::Url&& url_) noexcept
         : context_data{
               ::pltxt2htm::details::ParserFrameContextWithExternalTagInfo{.pltext = pltext_, .url = ::std::move(url_)}},
-          nested_tag_type{nested_tag_type_},
-          context_data_kind{ContextDataKind::external_tag} {
+          nested_tag_type{nested_tag_type_} {
+        bool const is_external_tag_type{
+            ::pltxt2htm::details::is_external_tag_type(nested_tag_type_)};
+        pltxt2htm_assert(is_external_tag_type, u8"mismatch node type");
     }
 
     constexpr explicit BasicFrameContext(::fast_io::u8string_view pltext_, ::pltxt2htm::NodeType const nested_tag_type_,
                                          ::std::size_t id_) noexcept
         : context_data{::pltxt2htm::details::ParserFrameContextWithPlSizeTagInfo{.pltext = pltext_, .id = id_}},
-          nested_tag_type{nested_tag_type_},
-          context_data_kind{ContextDataKind::pl_size_tag} {
+          nested_tag_type{nested_tag_type_} {
+        bool const is_pl_size_tag_type{
+            ::pltxt2htm::details::is_pl_size_tag_type(nested_tag_type_)};
+        pltxt2htm_assert(is_pl_size_tag_type, u8"mismatch node type");
     }
 
     constexpr explicit BasicFrameContext(::fast_io::u8string&& pltext_) noexcept
         : context_data{::pltxt2htm::details::ParserFrameContextWithMdBlockQuotesInfo{::std::move(pltext_)}},
-          nested_tag_type{::pltxt2htm::NodeType::md_block_quotes},
-          context_data_kind{ContextDataKind::md_block_quotes} {
+          nested_tag_type{::pltxt2htm::NodeType::md_block_quotes} {
     }
 
     constexpr explicit BasicFrameContext(::fast_io::u8string_view pltext_, ::pltxt2htm::Url&& link_) noexcept
         : context_data{
               ::pltxt2htm::details::ParserFrameContextWithMdLinkInfo{.pltext = pltext_, .link = ::std::move(link_)}},
-          nested_tag_type{::pltxt2htm::NodeType::md_link},
-          context_data_kind{ContextDataKind::md_link} {
+          nested_tag_type{::pltxt2htm::NodeType::md_link} {
     }
 
     constexpr explicit BasicFrameContext(::pltxt2htm::NodeType node_type,
                                          ::pltxt2htm::details::MdListAst&& md_list_ast_) noexcept
         : context_data{::pltxt2htm::details::ParserFrameContextWithMdListInfo{::std::move(md_list_ast_)}},
-          nested_tag_type{node_type},
-          context_data_kind{ContextDataKind::md_list} {
+          nested_tag_type{node_type} {
         pltxt2htm_assert(node_type == ::pltxt2htm::NodeType::md_ul || node_type == ::pltxt2htm::NodeType::md_ol,
                          u8"mismatch node type");
     }
@@ -210,68 +203,65 @@ public:
         : context_data{},
           nested_tag_type{other.nested_tag_type},
           current_index{other.current_index},
-          subast(::std::move(other.subast)),
-          context_data_kind{other.context_data_kind} {
-        switch (this->context_data_kind) {
-        case ContextDataKind::without_info:
-            break;
-        case ContextDataKind::pltext:
-            ::std::construct_at(::std::addressof(this->context_data.pltext), ::std::move(other.context_data.pltext));
-            break;
-        case ContextDataKind::equal_sign_tag:
+          subast(::std::move(other.subast)) {
+        if (::pltxt2htm::details::is_equal_sign_tag_type(this->nested_tag_type)) {
             ::std::construct_at(::std::addressof(this->context_data.equal_sign_tag),
                                 ::std::move(other.context_data.equal_sign_tag));
-            break;
-        case ContextDataKind::external_tag:
+        }
+        else if (::pltxt2htm::details::is_external_tag_type(this->nested_tag_type)) {
             ::std::construct_at(::std::addressof(this->context_data.external_tag),
                                 ::std::move(other.context_data.external_tag));
-            break;
-        case ContextDataKind::pl_size_tag:
+        }
+        else if (::pltxt2htm::details::is_pl_size_tag_type(this->nested_tag_type)) {
             ::std::construct_at(::std::addressof(this->context_data.pl_size_tag),
                                 ::std::move(other.context_data.pl_size_tag));
-            break;
-        case ContextDataKind::md_block_quotes:
+        }
+        else if (::pltxt2htm::details::is_md_block_quotes_type(this->nested_tag_type)) {
             ::std::construct_at(::std::addressof(this->context_data.md_block_quotes),
                                 ::std::move(other.context_data.md_block_quotes));
-            break;
-        case ContextDataKind::md_link:
+        }
+        else if (::pltxt2htm::details::is_md_link_type(this->nested_tag_type)) {
             ::std::construct_at(::std::addressof(this->context_data.md_link), ::std::move(other.context_data.md_link));
-            break;
-        case ContextDataKind::md_list:
+        }
+        else if (::pltxt2htm::details::is_md_list_type(this->nested_tag_type)) {
             ::std::construct_at(::std::addressof(this->context_data.md_list), ::std::move(other.context_data.md_list));
-            break;
+        }
+        else {
+            ::std::construct_at(::std::addressof(this->context_data.pltext), ::std::move(other.context_data.pltext));
         }
     }
 
-    constexpr auto operator=(BasicFrameContext const&) noexcept -> BasicFrameContext& = delete;
-    constexpr auto operator=(BasicFrameContext&&) noexcept -> BasicFrameContext& = delete;
+    constexpr auto operator=(::pltxt2htm::details::BasicFrameContext<ndebug> const&) noexcept
+        -> ::pltxt2htm::details::BasicFrameContext<ndebug>& = delete;
+    constexpr auto operator=(::pltxt2htm::details::BasicFrameContext<ndebug>&&) noexcept
+        -> ::pltxt2htm::details::BasicFrameContext<ndebug>& = delete;
 
     constexpr ~BasicFrameContext() noexcept {
-        switch (this->context_data_kind) {
-        case ContextDataKind::without_info:
-            return;
-        case ContextDataKind::pltext:
-            ::std::destroy_at(::std::addressof(this->context_data.pltext));
-            return;
-        case ContextDataKind::equal_sign_tag:
+        if (::pltxt2htm::details::is_equal_sign_tag_type(this->nested_tag_type)) {
             ::std::destroy_at(::std::addressof(this->context_data.equal_sign_tag));
             return;
-        case ContextDataKind::external_tag:
+        }
+        if (::pltxt2htm::details::is_external_tag_type(this->nested_tag_type)) {
             ::std::destroy_at(::std::addressof(this->context_data.external_tag));
             return;
-        case ContextDataKind::pl_size_tag:
+        }
+        if (::pltxt2htm::details::is_pl_size_tag_type(this->nested_tag_type)) {
             ::std::destroy_at(::std::addressof(this->context_data.pl_size_tag));
             return;
-        case ContextDataKind::md_block_quotes:
+        }
+        if (::pltxt2htm::details::is_md_block_quotes_type(this->nested_tag_type)) {
             ::std::destroy_at(::std::addressof(this->context_data.md_block_quotes));
             return;
-        case ContextDataKind::md_link:
+        }
+        if (::pltxt2htm::details::is_md_link_type(this->nested_tag_type)) {
             ::std::destroy_at(::std::addressof(this->context_data.md_link));
             return;
-        case ContextDataKind::md_list:
+        }
+        if (::pltxt2htm::details::is_md_list_type(this->nested_tag_type)) {
             ::std::destroy_at(::std::addressof(this->context_data.md_list));
             return;
         }
+        ::std::destroy_at(::std::addressof(this->context_data.pltext));
     }
 
     constexpr auto operator->() noexcept -> ::pltxt2htm::details::BasicFrameContext<ndebug>* {
@@ -296,57 +286,69 @@ public:
 
     [[nodiscard]]
     constexpr auto get_pltext() const noexcept -> ::fast_io::u8string_view {
-        switch (this->context_data_kind) {
-        case ContextDataKind::pltext:
-            return this->context_data.pltext.pltext;
-        case ContextDataKind::equal_sign_tag:
+        if (::pltxt2htm::details::is_equal_sign_tag_type(this->nested_tag_type)) {
             return this->context_data.equal_sign_tag.pltext;
-        case ContextDataKind::external_tag:
+        }
+        if (::pltxt2htm::details::is_external_tag_type(this->nested_tag_type)) {
             return this->context_data.external_tag.pltext;
-        case ContextDataKind::pl_size_tag:
+        }
+        if (::pltxt2htm::details::is_pl_size_tag_type(this->nested_tag_type)) {
             return this->context_data.pl_size_tag.pltext;
-        case ContextDataKind::md_block_quotes:
+        }
+        if (::pltxt2htm::details::is_md_block_quotes_type(this->nested_tag_type)) {
             return ::fast_io::u8string_view{this->context_data.md_block_quotes.pltext.data(),
                                             this->context_data.md_block_quotes.pltext.size()};
-        case ContextDataKind::md_link:
+        }
+        if (::pltxt2htm::details::is_md_link_type(this->nested_tag_type)) {
             return this->context_data.md_link.pltext;
-        case ContextDataKind::without_info:
-            [[fallthrough]];
-        case ContextDataKind::md_list:
+        }
+        if (::pltxt2htm::details::is_md_list_type(this->nested_tag_type)) {
             [[unlikely]] {
                 ::exception::unreachable<true>();
             }
         }
-        ::exception::unreachable<true>();
+        return this->context_data.pltext.pltext;
     }
 
     constexpr auto get_equal_sign_tag_id() noexcept -> ::fast_io::u8string& {
-        pltxt2htm_assert(this->context_data_kind == ContextDataKind::equal_sign_tag, u8"context kind mismatch");
+        bool const is_equal_sign_tag_type{
+            ::pltxt2htm::details::is_equal_sign_tag_type(this->nested_tag_type)};
+        pltxt2htm_assert(is_equal_sign_tag_type, u8"context kind mismatch");
         return this->context_data.equal_sign_tag.id;
     }
 
     constexpr auto get_external_tag_url() noexcept -> ::pltxt2htm::Url& {
-        pltxt2htm_assert(this->context_data_kind == ContextDataKind::external_tag, u8"context kind mismatch");
+        bool const is_external_tag_type{
+            ::pltxt2htm::details::is_external_tag_type(this->nested_tag_type)};
+        pltxt2htm_assert(is_external_tag_type, u8"context kind mismatch");
         return this->context_data.external_tag.url;
     }
 
     constexpr auto get_pl_size_tag_id() const noexcept -> ::std::size_t {
-        pltxt2htm_assert(this->context_data_kind == ContextDataKind::pl_size_tag, u8"context kind mismatch");
+        bool const is_pl_size_tag_type{
+            ::pltxt2htm::details::is_pl_size_tag_type(this->nested_tag_type)};
+        pltxt2htm_assert(is_pl_size_tag_type, u8"context kind mismatch");
         return this->context_data.pl_size_tag.id;
     }
 
     constexpr auto get_md_link_url() noexcept -> ::pltxt2htm::Url& {
-        pltxt2htm_assert(this->context_data_kind == ContextDataKind::md_link, u8"context kind mismatch");
+        bool const is_md_link_type{
+            ::pltxt2htm::details::is_md_link_type(this->nested_tag_type)};
+        pltxt2htm_assert(is_md_link_type, u8"context kind mismatch");
         return this->context_data.md_link.link;
     }
 
     constexpr auto get_md_list_ast() noexcept -> ::pltxt2htm::details::MdListAst& {
-        pltxt2htm_assert(this->context_data_kind == ContextDataKind::md_list, u8"context kind mismatch");
+        bool const is_md_list_type{
+            ::pltxt2htm::details::is_md_list_type(this->nested_tag_type)};
+        pltxt2htm_assert(is_md_list_type, u8"context kind mismatch");
         return this->context_data.md_list.md_list_ast;
     }
 
     constexpr auto get_md_list_iter() noexcept -> ::pltxt2htm::details::MdListAst::iterator& {
-        pltxt2htm_assert(this->context_data_kind == ContextDataKind::md_list, u8"context kind mismatch");
+        bool const is_md_list_type{
+            ::pltxt2htm::details::is_md_list_type(this->nested_tag_type)};
+        pltxt2htm_assert(is_md_list_type, u8"context kind mismatch");
         return this->context_data.md_list.iter;
     }
 };
