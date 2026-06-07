579c3ae5eff6674a498f2c5115a3743a93e396b6
Arendelle <Arendelle_@outlook.com>
2026-06-07 13:43:04 +0800

    add those files to formatter

--- cxxmodule/tests/check_public_symbols.cc
+++ cxxmodule/tests/check_public_symbols.cc
@@ -6,16 +6,14 @@
 auto get_header_symbols() -> ::std::vector<::std::string>;
 auto get_module_symbols() -> ::std::vector<::std::string>;
 
-int main()
-{
+int main() {
     auto header_vec = get_header_symbols();
     auto module_vec = get_module_symbols();
 
     ::std::sort(header_vec.begin(), header_vec.end());
     ::std::sort(module_vec.begin(), module_vec.end());
 
-    if (header_vec == module_vec)
-    {
+    if (header_vec == module_vec) {
         ::fast_io::println("PASS: all ", header_vec.size(), " public symbols match.");
         return 0;
     }
@@ -23,17 +21,13 @@ int main()
     ::fast_io::println("FAIL: symbol sets differ.");
     ::fast_io::println("  header: ", header_vec.size(), " symbols, module: ", module_vec.size(), " symbols");
 
-    for (auto const& s : header_vec)
-    {
-        if (!::std::binary_search(module_vec.cbegin(), module_vec.cend(), s))
-        {
+    for (auto const& s : header_vec) {
+        if (!::std::binary_search(module_vec.cbegin(), module_vec.cend(), s)) {
             ::fast_io::println("  only in header: ", s);
         }
     }
-    for (auto const& s : module_vec)
-    {
-        if (!::std::binary_search(header_vec.cbegin(), header_vec.cend(), s))
-        {
+    for (auto const& s : module_vec) {
+        if (!::std::binary_search(header_vec.cbegin(), header_vec.cend(), s)) {
             ::fast_io::println("  only in module: ", s);
         }
     }
--- cxxmodule/tests/module_symbols.cc
+++ cxxmodule/tests/module_symbols.cc
@@ -5,35 +5,28 @@
 import pltxt2htm;
 
 template<::std::meta::info ns>
-consteval auto collect_module_info() -> ::std::vector<::std::meta::info>
-{
+consteval auto collect_module_info() -> ::std::vector<::std::meta::info> {
     ::std::vector<::std::meta::info> symbols{};
-    template for (constexpr auto m :
-                 ::std::define_static_array(::std::meta::members_of(ns, ::std::meta::access_context::unprivileged())))
-    {
-        if constexpr (::std::meta::is_namespace(m))
-        {
-            if constexpr (!::std::meta::has_identifier(m))
-            {
+    template for (constexpr auto m : ::std::define_static_array(
+                      ::std::meta::members_of(ns, ::std::meta::access_context::unprivileged()))) {
+        if constexpr (::std::meta::is_namespace(m)) {
+            if constexpr (!::std::meta::has_identifier(m)) {
                 continue;
             }
             symbols.append_range(collect_module_info<m>());
         }
-        else
-        {
+        else {
             symbols.push_back(m);
         }
     }
     return symbols;
 }
 
-auto get_module_symbols() -> ::std::vector<::std::string>
-{
+auto get_module_symbols() -> ::std::vector<::std::string> {
     static constexpr auto info_arr = ::std::define_static_array(collect_module_info<^^::pltxt2htm>());
 
     ::std::vector<::std::string> result;
-    template for (constexpr auto& info : info_arr)
-    {
+    template for (constexpr auto& info : info_arr) {
         result.push_back(::std::string(::std::meta::identifier_of(info)));
     }
     return result;
