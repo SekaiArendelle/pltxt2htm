a873a3ffdc347fe7392b66e8d5a35237281b8024
Arendelle <Arendelle_@outlook.com>
2026-07-18 12:49:23 +0800

    test: more tests for common parser

--- tests/test_common_parser.cc
+++ tests/test_common_parser.cc
@@ -103,5 +103,211 @@ int main() {
         pltxt2htm_test_assert_equal(html, answer);
     }
 
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(u8"&amp;&'\"<>\t<br>\n");
+        auto answer = ::fast_io::u8string_view{u8"&amp;&amp;&apos;&quot;&lt;&gt;&nbsp;&nbsp;&nbsp;&nbsp;"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(
+            u8R"(\!\#\$\%\(\)\*\+\,\-\.\/\:\;\=\?\@\[\]\^\_\`\{\|\}\~\\\'\"\<\>\&)"
+            u8"escaped");
+        auto answer = ::fast_io::u8string_view{u8"!#$%()*+,-./:;=?@[]^_`{|}~\\&apos;&quot;&lt;&gt;&amp;escaped"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(u8"<span style=\"color:red\">text</span>");
+        auto answer = ::fast_io::u8string_view{u8"<span style=\"color:red;\">text</span>"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(u8"<span style=\"font-size:12px\">text</span>");
+        auto answer = ::fast_io::u8string_view{u8"<span style=\"font-size:12px;\">text</span>"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(u8"<span style=\"color:blue;font-size:16px\">text</span>");
+        auto answer = ::fast_io::u8string_view{u8"<span style=\"color:blue;font-size:16px;\">text</span>"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(u8"<a href=\"https://example.com\" internal>text</a>");
+        auto answer = ::fast_io::u8string_view{u8"<a href=\"https://example.com\" internal>text</a>"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(u8"__text__");
+        auto answer = ::fast_io::u8string_view{u8"<strong>text</strong>"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(u8"_text_");
+        auto answer = ::fast_io::u8string_view{u8"<em>text</em>"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(u8"<strong>text</strong>");
+        auto answer = ::fast_io::u8string_view{u8"<strong>text</strong>"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(u8"<em>text</em>");
+        auto answer = ::fast_io::u8string_view{u8"<em>text</em>"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(u8"<experiment=id>text</experiment>");
+        auto answer = ::fast_io::u8string_view{u8"text"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(u8"<discussion=id>text</discussion>");
+        auto answer = ::fast_io::u8string_view{u8"text"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(u8"<user=id>text</user>");
+        auto answer = ::fast_io::u8string_view{u8"text"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(u8"<size=12>text</size>");
+        auto answer = ::fast_io::u8string_view{u8"text"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(u8"<p>text</p>");
+        auto answer = ::fast_io::u8string_view{u8"text"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html =
+            ::pltxt2htm_test::pltxt2common_htmld(u8"<h1>1</h1><h2>2</h2><h3>3</h3><h4>4</h4><h5>5</h5><h6>6</h6>");
+        auto answer = ::fast_io::u8string_view{u8"123456"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(u8"<del>text</del>");
+        auto answer = ::fast_io::u8string_view{u8"text"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(u8"<ul><li>u</li></ul><ol><li>o</li></ol>");
+        auto answer = ::fast_io::u8string_view{u8"uo"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html =
+            ::pltxt2htm_test::pltxt2common_htmld(u8"<code>code</code><pre>pre</pre><blockquote>quote</blockquote>");
+        auto answer = ::fast_io::u8string_view{u8"codeprequote"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(
+            u8"<table><caption>c</caption><colgroup><col></colgroup><thead><tr><th>h</th></tr></"
+            u8"thead><tbody><tr><td>d</"
+            u8"td></tr></tbody><tfoot><tr><td>f</td></tr></tfoot></table>");
+        auto answer = ::fast_io::u8string_view{u8"chdf"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(
+            u8"<hr><input type=\"checkbox\" disabled><img src=\"a.png\" alt=\"a\"><!-- note -->");
+        auto answer = ::fast_io::u8string_view{u8""};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(u8"~~text~~");
+        auto answer = ::fast_io::u8string_view{u8"text"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(u8"- item");
+        auto answer = ::fast_io::u8string_view{u8"item"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(u8"1. item");
+        auto answer = ::fast_io::u8string_view{u8"item"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(u8"- [x] item");
+        auto answer = ::fast_io::u8string_view{u8"item"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(u8"# 1\n## 2\n### 3\n#### 4\n##### 5\n###### 6");
+        auto answer = ::fast_io::u8string_view{u8"123456"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(u8"> quote");
+        auto answer = ::fast_io::u8string_view{u8"quote"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(u8"| h |\n|-|\n| d |");
+        auto answer = ::fast_io::u8string_view{u8"hd"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(u8"`one```two`````three```");
+        auto answer = ::fast_io::u8string_view{u8"onetwothree"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(u8"```\ncode\n```\n~~~\nmore\n~~~");
+        auto answer = ::fast_io::u8string_view{u8"codemore"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(u8"$inline$");
+        auto answer = ::fast_io::u8string_view{u8"inline"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(u8"$$block$$");
+        auto answer = ::fast_io::u8string_view{u8"block"};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
+    {
+        auto html = ::pltxt2htm_test::pltxt2common_htmld(u8"---");
+        auto answer = ::fast_io::u8string_view{u8""};
+        pltxt2htm_test_assert_equal(html, answer);
+    }
+
     return 0;
 }
