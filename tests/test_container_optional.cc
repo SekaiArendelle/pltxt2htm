511f0666c0e23e6fdfa1a129d6fa01a9ae2afbb5
Arendelle <Arendelle_@outlook.com>
2026-09-01 12:48:25 +0800

    feat: support reference Optional values

--- tests/test_container_optional.cc
+++ tests/test_container_optional.cc
@@ -97,6 +97,75 @@ consteval bool optional_non_zero_constexpr_operations_work() noexcept {
 
 static_assert(optional_non_zero_constexpr_operations_work());
 
+using IntReferenceOptional = ::pltxt2htm::container::Optional<int&>;
+using ConstIntReferenceOptional = ::pltxt2htm::container::Optional<int const&>;
+
+static_assert(::pltxt2htm::container::is_optional<IntReferenceOptional>);
+static_assert(!::std::default_initializable<IntReferenceOptional>);
+static_assert(::std::same_as<IntReferenceOptional::value_type, int>);
+static_assert(::std::same_as<IntReferenceOptional::rebind<double&>, ::pltxt2htm::container::Optional<double&>>);
+static_assert(sizeof(IntReferenceOptional) == sizeof(int*));
+static_assert(alignof(IntReferenceOptional) == alignof(int*));
+static_assert(::std::is_trivially_copyable_v<IntReferenceOptional>);
+static_assert(::std::is_constructible_v<IntReferenceOptional, int&>);
+static_assert(!::std::is_constructible_v<IntReferenceOptional, int&&>);
+static_assert(::std::is_constructible_v<ConstIntReferenceOptional, int&>);
+static_assert(::std::is_constructible_v<ConstIntReferenceOptional, int const&>);
+static_assert(!::std::is_constructible_v<ConstIntReferenceOptional, int&&>);
+static_assert(!::std::is_constructible_v<ConstIntReferenceOptional, int const&&>);
+static_assert(::std::is_assignable_v<IntReferenceOptional&, int&>);
+static_assert(!::std::is_assignable_v<IntReferenceOptional&, int&&>);
+static_assert(!::std::is_assignable_v<IntReferenceOptional&&, int&>);
+
+static_assert(::std::same_as<
+              decltype(::std::declval<IntReferenceOptional&>().value<::pltxt2htm::Contracts::quick_enforce>()), int&>);
+static_assert(
+    ::std::same_as<
+        decltype(::std::declval<IntReferenceOptional const&>().value<::pltxt2htm::Contracts::quick_enforce>()), int&>);
+static_assert(::std::same_as<
+              decltype(::std::declval<IntReferenceOptional&&>().value<::pltxt2htm::Contracts::quick_enforce>()), int&>);
+static_assert(
+    ::std::same_as<
+        decltype(::std::declval<IntReferenceOptional const&&>().value<::pltxt2htm::Contracts::quick_enforce>()), int&>);
+static_assert(::std::same_as<decltype(::std::declval<IntReferenceOptional const&>().value_or(0)), int>);
+
+consteval bool optional_reference_constexpr_operations_work() noexcept {
+    int first{7};
+    int second{11};
+    int equal_to_first{7};
+    IntReferenceOptional value{first};
+    IntReferenceOptional empty{::pltxt2htm::container::nullopt};
+    IntReferenceOptional equal_value{equal_to_first};
+    if (!value.has_value() || empty.has_value() || value != equal_value) {
+        return false;
+    }
+    value.value<::pltxt2htm::Contracts::ignore>() = 9;
+    if (first != 9 || value.value_or(0) != 9) {
+        return false;
+    }
+    empty = value;
+    if (::std::addressof(empty.value<::pltxt2htm::Contracts::ignore>()) != ::std::addressof(first)) {
+        return false;
+    }
+    value = second;
+    if (::std::addressof(value.value<::pltxt2htm::Contracts::ignore>()) != ::std::addressof(second) || first != 9) {
+        return false;
+    }
+    static_cast<IntReferenceOptional const&>(value).value<::pltxt2htm::Contracts::ignore>() = 13;
+    if (second != 13) {
+        return false;
+    }
+    value.swap(empty);
+    if (::std::addressof(value.value<::pltxt2htm::Contracts::ignore>()) != ::std::addressof(first) ||
+        ::std::addressof(empty.value<::pltxt2htm::Contracts::ignore>()) != ::std::addressof(second)) {
+        return false;
+    }
+    value = ::pltxt2htm::container::nullopt;
+    return value == ::pltxt2htm::container::nullopt && empty == 13;
+}
+
+static_assert(optional_reference_constexpr_operations_work());
+
 namespace pltxt2htm_test {
 
 struct OptionalTrackedValue {
@@ -151,6 +220,16 @@ int main() {
                                ::std::addressof(value.value<::pltxt2htm::Contracts::quick_enforce>()));
     pltxt2htm_test_assert_true(static_cast<IntOptional const&>(empty).value_or(const_fallback) == const_fallback);
     pltxt2htm_test_assert_true(IntOptional{::pltxt2htm::container::nullopt}.value_or(int{11}) == 11);
+
+    int first_reference_value{21};
+    int second_reference_value{34};
+    IntReferenceOptional reference{first_reference_value};
+    reference.value<::pltxt2htm::Contracts::quick_enforce>() = 22;
+    pltxt2htm_test_assert_true(first_reference_value == 22);
+    reference = second_reference_value;
+    pltxt2htm_test_assert_true(::std::addressof(reference.value<::pltxt2htm::Contracts::quick_enforce>()) ==
+                               ::std::addressof(second_reference_value));
+    pltxt2htm_test_assert_true(first_reference_value == 22);
 
     using TrackedOptional = ::pltxt2htm::container::Optional<::pltxt2htm_test::OptionalTrackedValue>;
 
