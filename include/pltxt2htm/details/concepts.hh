31fd01c7b37b01d8662ff010e539b1b16d4130a1
Arendelle <Arendelle_@outlook.com>
2026-09-06 15:48:22 +0800

    can I remove my custom fallback impl for reference_constructts_from_temporary_v?

--- include/pltxt2htm/details/concepts.hh
+++ include/pltxt2htm/details/concepts.hh
@@ -14,22 +14,17 @@ concept is_char_type = ::std::same_as<T, char> || ::std::same_as<T, wchar_t> || 
                        ::std::same_as<T, char16_t> || ::std::same_as<T, char32_t>;
 
 template<typename T, typename U>
-constexpr bool reference_constructs_from_temporary_fallback_v =
-    !::std::is_lvalue_reference_v<U> || !::std::is_convertible_v<::std::add_pointer_t<::std::remove_reference_t<U>>,
-                                                                 ::std::add_pointer_t<::std::remove_reference_t<T>>>;
-
-template<typename T, typename U>
 concept reference_constructs_from_temporary =
-#if defined(__cpp_lib_reference_from_temporary)
+#if __cpp_lib_reference_from_temporary >= 202202L
     ::std::reference_constructs_from_temporary_v<T, U>;
 #elif defined(__has_builtin)
     #if __has_builtin(__reference_constructs_from_temporary)
     __reference_constructs_from_temporary(T, U);
     #else
-    reference_constructs_from_temporary_fallback_v<T, U>;
+        #error "compiler too old for __reference_constructs_from_temporary"
     #endif
 #else
-    reference_constructs_from_temporary_fallback_v<T, U>;
+    #error "__has_builtin is not supported; compiler too old for reference_constructs_from_temporary_v"
 #endif
 
 } // namespace pltxt2htm::details
