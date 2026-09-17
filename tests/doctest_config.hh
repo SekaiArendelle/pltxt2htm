ebcd566c8c81cf9dab03b76b4b7d777f4fd3c1e2
Arendelle <Arendelle_@outlook.com>
2026-09-18 07:37:55 +0800

    update doctest to v2.5.3

--- tests/doctest_config.hh
+++ tests/doctest_config.hh
@@ -1,11 +1,4 @@
 #pragma once
 
-#if defined(__clang__)
-    #pragma clang attribute push(__attribute__((no_sanitize("memory"))), apply_to = any(function))
-#endif
 #include <doctest/doctest.h>
-#if defined(__clang__)
-    #pragma clang attribute pop
-#endif
-
 #include "precompile.hh"
